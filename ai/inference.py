d12a851cee9fce244ba92fcd91d412f5235bf746
JoshuaPlacidi <JoshuaPlacidi@gmail.com>
2023-06-09 16:40:33 +0100

    added generate latents function skeleton

--- ai/inference.py
+++ ai/inference.py
@@ -1,11 +1,12 @@
-
-
-def encode(model_weights, image_path):
+def encode(model_weights_path, image_path):
     # TODO takes a image_path and returns the laten representation
 
     return
 
-def decode(model_weights, latents):
+def decode(model_weights_path, latents):
     # TODO takes a latent representation and returns an image
 
     return
+
+def generate_and_store_latents(model_weights_path, image_paths: list[str], save_path: str)
+    # TODO takes a list of image paths, passes them through the encoder, compresses the latents to 2D and stores at save_path
